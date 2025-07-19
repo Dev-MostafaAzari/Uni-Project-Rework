98c9f4d4982c50b2371cc90c7427959412f44426
Mostafa <msari808755@gmail.com>
2025-07-19 11:09:29 -0700

    mainPagePart1

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import mainImg from "../assets/6.jpg";
+import "../styles/MainPage.css"
 
 
 function MainPage(){
     return(
-        <div>
-
+        <div className="mainPageContent">
+            <div className="mainImgDiv">
+                <img id="mainImg" src={mainImg} alt="mainImg"/>
+                <div className="textMainImg">
+                    <h3 className="Img-h3">Shopping Site</h3>
+                    <p>This website was created by<a href="https://github.com/Dev-MostafaAzari">MostafaAzari</a></p>
+                </div>
+            </div>
         </div>
     )
 }
