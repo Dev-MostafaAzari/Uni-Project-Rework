a408bef9e2988b532809efb6ffd2dd8abf3ca51d
Mostafa <msari808755@gmail.com>
2025-07-20 07:05:38 -0700

    fixingNavbar/MainCategoryStyle

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,18 +4,18 @@ import CloseImg from "../assets/clothes.jpg";
 import JewelryImg from "../assets/jewelry.jpg";
 import ElectronicImg from "../assets/electronics.jpg";
 import "../styles/MainPage.css"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faGithub} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { Col, Row } from 'react-bootstrap';
 
-function MainPage(){
-    return(
+function MainPage() {
+    return (
         <div className="mainPageContent">
             <div className="mainImgDiv">
-                <img id="mainImg" src={mainImg} alt="mainImg"/>
+                <img id="mainImg" src={mainImg} alt="mainImg" />
                 <div className="textMainImg">
                     <h3 className="Img-h3">Shopping Site</h3>
-                    <p>This website was created by<a href="https://github.com/Dev-MostafaAzari">MostafaAzari<FontAwesomeIcon icon={faGithub}/></a></p>
+                    <p>This website was created by<a href="https://github.com/Dev-MostafaAzari">MostafaAzari<FontAwesomeIcon icon={faGithub} /></a></p>
                 </div>
             </div>
             <div className="CategoryMainDiv">
@@ -25,22 +25,28 @@ function MainPage(){
                 <Col className="CategoryMainItems col-12">
                     <Row className="CategoryMainContainer" xxl={4} xl={4} lg={3} md={2} sm={1} xs={1}>
                         <Col>
-                            <div className="CategoryMainCards">
-                                <img className="CategoryMainImg" src={CloseImg} alt="Clothes"/>
-                                <span className="CategoryMainSpan">Clothes</span>
-                            </div>
+                            <a href="#">
+                                <div className="CategoryMainCards">
+                                    <img className="CategoryMainImg" src={CloseImg} alt="Clothes" />
+                                    <span className="CategoryMainSpan">Clothes</span>
+                                </div>
+                            </a>
                         </Col>
                         <Col>
-                            <div className="CategoryMainCards">
-                                <img className="CategoryMainImg" src={ElectronicImg} alt="Clothes"/>
-                                <span className="CategoryMainSpan">Electronics</span>
-                            </div>
+                            <a href="#">
+                                <div className="CategoryMainCards">
+                                    <img className="CategoryMainImg" src={ElectronicImg} alt="Clothes" />
+                                    <span className="CategoryMainSpan">Electronics</span>
+                                </div>
+                            </a>
                         </Col>
                         <Col>
-                            <div className="CategoryMainCards">
-                                <img className="CategoryMainImg" src={JewelryImg} alt="Clothes"/>
-                                <span className="CategoryMainSpan">Jewelrys</span>
-                            </div>
+                            <a href="#">
+                                <div className="CategoryMainCards">
+                                    <img className="CategoryMainImg" src={JewelryImg} alt="Clothes" />
+                                    <span className="CategoryMainSpan">Jewelrys</span>
+                                </div>
+                            </a>
                         </Col>
                     </Row>
                 </Col>
