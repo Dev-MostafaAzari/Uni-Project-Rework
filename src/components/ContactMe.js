67f973bcf97a460e6814d3c7f8b5d9622d418b62
Mostafa <msari808755@gmail.com>
2025-07-24 08:32:31 -0700

    SocialLinks

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Button, Form } from 'react-bootstrap';
 import "../styles/ContactMe.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub,faTelegram,faInstagram } from '@fortawesome/free-brands-svg-icons';
+
 
 function Contactme() {
     return (
@@ -9,6 +12,11 @@ function Contactme() {
                 <div className="textPartContainer">
                     <h3 className="ContactTitle">Get in Touch</h3>
                     <p className="ContactText">if you have any inquiries or just want to say hi,please use our contact form </p>
+                    <div className="SocialLinksContainer">
+                        <a className="SocialLinks" href="https://github.com/Dev-MostafaAzari" target="_blank"><FontAwesomeIcon icon={faGithub}/></a>
+                        <a className="SocialLinks" href="#" target="_blank"><FontAwesomeIcon icon={faInstagram}/></a>
+                        <a className="SocialLinks" href="#" target="_blank"><FontAwesomeIcon icon={faTelegram}/></a>
+                    </div>
                 </div>
             </div>
             <div className="inputPartContainer">
