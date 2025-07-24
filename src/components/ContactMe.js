eb05a8d627f6884041003f204362e2d8c79e249d
Mostafa <msari808755@gmail.com>
2025-07-24 08:17:56 -0700

    ContactME/Styling

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -6,7 +6,10 @@ function Contactme() {
     return (
         <div className="mainContentContainer">
             <div className="linkPartContainer">
-                <h3 className="ContactTitle">Get in Touch</h3>
+                <div className="textPartContainer">
+                    <h3 className="ContactTitle">Get in Touch</h3>
+                    <p className="ContactText">if you have any inquiries or just want to say hi,please use our contact form </p>
+                </div>
             </div>
             <div className="inputPartContainer">
                 <Form className="formContact">
@@ -26,12 +29,12 @@ function Contactme() {
                             <Form.Label>Email</Form.Label>
                         </Form.Floating>
                         <Form.Floating>
-                            <Form.Control type="text" className="inputFormContact" placeholder="" />
+                            <Form.Control type="text" id="ContactMessage" className="inputFormContact" placeholder="" />
                             <Form.Label>Your Message</Form.Label>
                         </Form.Floating>
                     </div>
                     <div className="formContact3">
-                        <Button type="button">Send</Button>
+                        <Button className="SendDataContent" type="button">Send</Button>
                     </div>
                 </Form>
             </div>
