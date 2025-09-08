34134c990f069901c404cce64acf2b8f3f6474f9
Mostafa <msari808755@gmail.com>
2025-09-08 05:59:32 -0700

    loginIconChangeOnLogin

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React, { useContext } from 'react';
-import { Container, Nav, Navbar, NavbarBrand, NavbarCollapse, NavbarToggle } from "react-bootstrap";
+import { Nav, Navbar, NavbarBrand, NavbarCollapse, NavbarToggle } from "react-bootstrap";
 import myLogo from "../assets/dev-mostafa-2.jpg";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faShoppingCart, faUserCircle} from "@fortawesome/free-solid-svg-icons";
+import {faShoppingCart, faSignIn, faUserCircle} from "@fortawesome/free-solid-svg-icons";
 import "../styles/Navbar.css";
 import { Link } from 'react-router-dom';
 import { LoginOffCanContext } from '../contexts/LoginAndOffcanContext';
@@ -19,7 +19,7 @@ function NavbarPart() {
                     <NavbarToggle />
                     <NavbarCollapse>
                         <Nav>
-                            <Nav.Item>{profile ? <button type="button" onClick={Open} className="fonts"><FontAwesomeIcon icon={faUserCircle}/></button> : <Link to="/login" className="fontsLink"><FontAwesomeIcon icon={faUserCircle}/></Link>}</Nav.Item>
+                            <Nav.Item>{profile ? <button type="button" onClick={Open} className="fonts"><FontAwesomeIcon icon={faUserCircle}/></button> : <Link to="/login" className="fontsLink"><FontAwesomeIcon icon={faSignIn}/></Link>}</Nav.Item>
                             <Nav.Item><button type="button" className="fonts"><FontAwesomeIcon icon={faShoppingCart}/></button></Nav.Item>
                             <Link to="/Uni-Project-Rework">Home</Link>
                             <Link to="/products">Products</Link>
